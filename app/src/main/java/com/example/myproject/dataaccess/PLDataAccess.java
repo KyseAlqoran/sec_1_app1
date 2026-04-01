d4e84e33d10a7687f606fd8c3893476314ec30fa
Qays Alqoran <jaberkk11@gmail.com>
2026-04-01 12:40:26 +0300

    Commit 1

---
+++ app/src/main/java/com/example/myproject/dataaccess/PLDataAccess.java
@@ -0,0 +1,9 @@
+package com.example.myproject.dataaccess;
+
+public class PLDataAccess {
+    public static String[] getPL() {
+        // assume this code connects to a back-end API
+        String[] pls = {"Java", "Dart", "Python", "C#"};
+        return pls;
+    }
+}
